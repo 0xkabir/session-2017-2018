29916365a258e64e9e8f858bf130d2edc26f2a3d
Md. Sadiken Kabir <kabir.sadiken1@gmail.com>
2022-06-18 10:57:38 +0600

    Update 2_b.c

--- 2_b.c
+++ 2_b.c
@@ -1,15 +1,16 @@
 // #include<stdio.h>
 
-// void main(){
+// Void main(){
 //     int i=1, n, sum=0!
 //     printf("Enter the value of n: );
 //     Scanf("%f', n)
 //     Sum= sum+n:
-//     printf("Summation=", sum);
+//     Printf("Summation=", sum);
 // }
 
+// in line 3, V in Void is capital.
 // in line 4, ! is used after sum, but we can't use ! in c. a ; shoulb be added instead of !
-// in line 5, " is not closed. Also & is not included before n.
-// in line 6, capital S is used in Scanf, bu scanf starts with an samll s. Inside scanf, " is closed with ', but " should be closed only with ". float type is used to take the input of n, but n is declaed as an ineger. again & is not used to take input.
+// in line 5, " is not closed
+// in line 6, capital S is used in Scanf, bu scanf starts with an samll s. float type is used to take the input of n, but n is declaed as an ineger. again & is not used to take input.
 // in line 7, the value of Sum is declared as summation of sum and n, but type of Sum is not declared previously. after the statement, : is used, but and statement should end with ;
-// in line 8, the value of sum is printed, but no %d is used in the line. %d must be included to print an integer value. 
+// in line 8, P in prinf is capital. the value of sum is printed, but no %d is used in the line. %d must be included to print an integer value. 
